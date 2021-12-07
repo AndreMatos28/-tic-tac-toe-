be5227432bfd13f6c854ed0a86121c992989cff5
AndreMatos28 <andrealleati@gmail.com>
2021-12-07 13:26:42 -0300

    jogo pronto

--- scripts/interface.js
+++ scripts/interface.js
@@ -1,28 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
-    let squares = document.querySelectorAll(".square");
+  let squares = document.querySelectorAll(".square");
 
-    squares.forEach((square) => {
-        square.addEventListener("click", handleClick);
-        })
-    })
+  squares.forEach((square) => {
+    square.addEventListener("click", handleClick);
+  });
+});
 
 function handleClick(event) {
+  if (handleMove(event.target.id)){
+    setTimeout(() => {
+        alert ("Ganhador " + playerTime);
+    }, 10);
+  };
 
-    handleMove(event.target.id); 
-    updateSquares();
+ updateSquares(event.target.id);
 }
 
-
-function updateSquares() {
-    let squares = document.querySelectorAll(".square");
-
-    squares.forEach((square) => {
-        let symbol = board[square.id]; 
-
-        if (symbol != ""){
-            square.innerHTML = `<div class="${symbol}"></div>`
-        }
-
-    })
+function updateSquares(position) {
+    let square = document.getElementById(position.toString());
+    let symbol = board[position];
+    square.innerHTML = `<div class="${symbol}"></div>`;
 }
+
+// function updateSquares() {
+//   let squares = document.querySelectorAll(".square");
+
+//   squares.forEach((square) => {
+//     let symbol = board[square.id];
+
+//     if (symbol != "") {
+//       square.innerHTML = `<div class="${symbol}"></div>`;
+//     }
+//   });
+// }
