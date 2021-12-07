0471e67e4e4f5ecd0f40864398bdf11710e5eedc
AndreMatos28 <andrealleati@gmail.com>
2021-12-07 15:14:42 -0300

    add começar, reiniciar, emojis e indicador

--- scripts/game.js
+++ scripts/game.js
@@ -49,3 +49,4 @@ function isWin() {
   }
   return false;
 }
+
--- scripts/interface.js
+++ scripts/interface.js
@@ -1,3 +1,5 @@
+playerTimer();
+
 document.addEventListener("DOMContentLoaded", () => {
   let squares = document.querySelectorAll(".square");
 
@@ -9,7 +11,12 @@ document.addEventListener("DOMContentLoaded", () => {
 function handleClick(event) {
   if (handleMove(event.target.id)){
     setTimeout(() => {
-        alert ("Ganhador " + playerTime);
+        if (playerTime == 0 ){
+            alert ("Ganhador  ✖️");
+        } else {
+            alert ("Ganhador  ⭕");            
+        }
+       
     }, 10);
   };
 
@@ -20,16 +27,45 @@ function updateSquares(position) {
     let square = document.getElementById(position.toString());
     let symbol = board[position];
     square.innerHTML = `<div class="${symbol}"></div>`;
+    playerTimer();
 }
 
-// function updateSquares() {
-//   let squares = document.querySelectorAll(".square");
+function updateSquaresReset() {
+  let squares = document.querySelectorAll(".square");
 
-//   squares.forEach((square) => {
-//     let symbol = board[square.id];
+  squares.forEach((square) => {
+    let symbol = board[square.id];
 
-//     if (symbol != "") {
-//       square.innerHTML = `<div class="${symbol}"></div>`;
-//     }
-//   });
-// }
+    if (symbol == "") {
+      square.innerHTML = `<div class=""></div>`;
+    }
+  });
+  playerTimer();
+}
+
+function init(){
+    document.getElementsByClassName("init")[0].style.display = "none";
+    document.getElementsByClassName("stage")[0].style.display = "block";
+   
+}
+
+function reset(){
+    board = ["", "", "", "", "", "", "", "", ""];
+    playerTime = 0;
+    symbols = ["o", "x"];
+    gameOver = false;
+
+    updateSquaresReset();
+}
+
+function playerTimer(){
+    if(gameOver){  
+        return;
+    }
+
+    if (playerTime == 0 ){
+        document.getElementById("h1").innerHTML = "VEZ DO JOGADOR ✖️";
+    } else {
+        document.getElementById("h1").innerHTML = "VEZ DO JOGADOR ⭕";
+    }
+}
